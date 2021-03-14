ce23768d8130f97ed6608f175e1d18cab43a6d41
Den <kdv2kz@yandex.ru>
2021-03-14 23:33:17 +0600

    edit on comments

--- 01-introduction-to-spring/src/main/java/ru/otus/homework/settings/xml/cvs/CSVWorkImpl.java
+++ 01-introduction-to-spring/src/main/java/ru/otus/homework/settings/xml/cvs/CSVWorkImpl.java
@@ -1,18 +1,18 @@
 package ru.otus.homework.settings.xml.cvs;
 
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
+@Component("csvWork")
 public class CSVWorkImpl implements CSVWork{
-    private final File file;
-
-    public CSVWorkImpl(File file) {
-        this.file = file;
-    }
+    @Value("${file.name}")
+    private File file;
 
     @Override
     public List<CVSRow> getCSVRows() throws IOException {
