a099e458c083696aecbebe62e0e42c04542ef5cb
Den <kdv2kz@yandex.ru>
2021-03-15 00:02:46 +0600

    edit on comments

---
+++ 01-introduction-to-spring/src/test/java/ru/otus/homework/settings/xml/test/PersonDAOImplTest.java
@@ -0,0 +1,39 @@
+package ru.otus.homework.settings.xml.test;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.otus.homework.settings.xml.Application;
+import ru.otus.homework.settings.xml.dao.PersonDAOImpl;
+import ru.otus.homework.settings.xml.domain.Person;
+
+import java.io.IOException;
+
+@DisplayName("class - PersonDAOImplTest")
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = Application.class)
+public class PersonDAOImplTest {
+
+    @Autowired
+    private PersonDAOImpl personDAO;
+
+    @Test
+    @DisplayName("при вызове не должно быть ошибки")
+    void notExceptionOnCallPersonByName () {
+        Assertions.assertDoesNotThrow(() -> personDAO.getPersonByName(null));
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("если персона не загружена - ошибка")
+    void getPersonByNameTest () {
+        Person person = personDAO.getPersonByName(null);
+        Assertions.assertNotNull(person);
+    }
+
+}
