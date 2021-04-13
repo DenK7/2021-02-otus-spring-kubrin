98ef1250d369092971147e99e67833f704d564cb
Den <kdv2kz@yandex.ru>
2021-04-14 00:35:38 +0600

    spring-boot

--- 02-magic-spring-boot/src/test/java/ru/otus/homework/magic/spring/boot/LoaderCSVImplTest.java
+++ 02-magic-spring-boot/src/test/java/ru/otus/homework/magic/spring/boot/LoaderCSVImplTest.java
@@ -3,13 +3,11 @@ package ru.otus.homework.magic.spring.boot;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.otus.homework.magic.spring.boot.exception.LoadFileException;
 import ru.otus.homework.magic.spring.boot.loader.Loader;
 import ru.otus.homework.magic.spring.boot.loader.LoaderCSVImpl;
-import ru.otus.homework.magic.spring.boot.parser.ParserCSVImpl;
 
 @DisplayName("class - LoaderCSVImplTest")
 @SpringBootTest
--- 02-magic-spring-boot/src/test/java/ru/otus/homework/magic/spring/boot/ParserCSVImplTest.java
+++ 02-magic-spring-boot/src/test/java/ru/otus/homework/magic/spring/boot/ParserCSVImplTest.java
@@ -3,10 +3,8 @@ package ru.otus.homework.magic.spring.boot;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.otus.homework.magic.spring.boot.exception.ParsException;
 import ru.otus.homework.magic.spring.boot.parser.Parser;
 
 @DisplayName("class - ParserCSVImplTest")
@@ -18,7 +16,7 @@ public class ParserCSVImplTest {
 
     @Test
     @DisplayName("проверка ошибки - если файл не пришел")
-    void testErrIfFileIsNull() throws ParsException {
+    void testErrIfFileIsNull() {
         Assertions.assertThrows(NullPointerException.class, () -> parser.getParsRows(null));
     }
 }
