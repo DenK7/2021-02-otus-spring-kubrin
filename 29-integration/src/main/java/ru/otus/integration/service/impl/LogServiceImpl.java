7cfd535788582424bf4087ebb3849e90ed3f2c64
Den <kdv2kz@yandex.ru>
2021-07-22 01:11:06 +0600

    logfile

--- 29-integration/src/main/java/ru/otus/integration/service/impl/LogServiceImpl.java
+++ 29-integration/src/main/java/ru/otus/integration/service/impl/LogServiceImpl.java
@@ -1,27 +1,32 @@
 package ru.otus.integration.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.otus.integration.model.PeopleModel;
 import ru.otus.integration.service.api.LogService;
 
 @Service
 public class LogServiceImpl implements LogService {
+    Logger logger = LoggerFactory.getLogger(LogServiceImpl.class);
+
+    // добавил логгер для отслеживания настроек в application.yml, а именно ru.otus: error
 
     @Override
     public void writeLogSMS(PeopleModel peopleModel) {
-        System.out.println("Write to log about messages to " + peopleModel.getHumanName() +
+        logger.info( "Write to log about messages to " + peopleModel.getHumanName() +
                 ", message -> " + peopleModel.getMessageValue() +", sms message");
     }
 
     @Override
     public void writeLogPush(PeopleModel peopleModel) {
-        System.out.println("Write to log about messages to " + peopleModel.getHumanName() +
+        logger.warn("Write to log about messages to " + peopleModel.getHumanName() +
                 ", message -> " + peopleModel.getMessageValue() + ", push message");
     }
 
     @Override
     public void writeLogEMail(PeopleModel peopleModel) {
-        System.out.println("Write to log about messages to " + peopleModel.getHumanName() +
+        logger.error("Write to log about messages to " + peopleModel.getHumanName() +
                 ", message -> " + peopleModel.getMessageValue() + ", email");
     }
 }
